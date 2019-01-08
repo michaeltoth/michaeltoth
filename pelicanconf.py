c5142b4a466362ed5064c0fe8b2396cacccb8afb
Michael Toth <mt.toth@gmail.com>
2019-01-08 14:14:12 -0500

    Updating copyright, site description, and links

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,8 +5,8 @@ from __future__ import unicode_literals
 AUTHOR = u'Michael Toth'
 SITENAME = u"Michael Toth's Blog"
 SITETITLE = AUTHOR
-SITESUBTITLE = u'Data Scientist, R Programmer'
-SITEDESCRIPTION = u'%s\'s Thoughts and Writings on Statistics, Data Science, Programming, and Economics' % AUTHOR
+SITESUBTITLE = u'Consulting Data Scientist and Writer'
+SITEDESCRIPTION = u'%s is a data scientist who specializes in writing analytical content that helps financial technology businesses win more customers. This blog hosts Michael\'s thoughts and writings on statistics, data science, programming, finance, and economics' % AUTHOR
 SITEURL = 'http://localhost:8000'
 SITELOGO = SITEURL + '/images/profile.jpeg'
 FAVICON = SITEURL + '/images/favicon.ico' 
@@ -19,7 +19,7 @@ CC_LICENSE = {
         'version': '4.0',
         'slug': 'by-sa'
 }
-COPYRIGHT_YEAR = 2017
+COPYRIGHT_YEAR = 2019
 
 
 THEME = u"../Flex"
@@ -45,5 +45,8 @@ SOCIAL = (('twitter', 	'https://twitter.com/Michael_Toth'),
 MENUITEMS = (('Archives', '/archives.html'),
             ('Categories', '/categories.html'),
 	    ('Tags', '/tags.html'),)
+
+LINKS = (('My Artwork', 'http://www.artfuldataprints.com'),
+         ('Work With Me', 'https://www.michaeltothanalytics.com'))
 
 DEFAULT_PAGINATION = 10
