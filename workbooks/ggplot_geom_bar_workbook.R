e58aad38411d8b643794aeae7e4c5f1be34d1251
Michael Toth <mt.toth@gmail.com>
2019-04-30 11:50:30 -0400

    Finalizing geom_bar workbook

--- workbooks/ggplot_geom_bar_workbook.R
+++ workbooks/ggplot_geom_bar_workbook.R
@@ -1,6 +1,4 @@
-# STATUS: IN PROGRESS, DO NOT USE
-
-# This workbook is built to help you master the scatter plot in ggplot
+# This workbook is built to help you master the bar chart in ggplot
 # It is designed to accompany this guide on my website:
 # https://michaeltoth.me/a-detailed-guide-to-the-ggplot-scatter-plot-in-r.html
 
@@ -17,30 +15,30 @@ library(tidyverse)
 
 # 2. Investigate the dataset
 
-head(mtcars)
-str(mtcars)
+head(mpg)
+str(mpg)
 
 # QUESTION: What variables are in the data, and what values do you see?
 
 
 
 ################################################################################
-############################ Creating Scatter Plots ############################
+############################## Creating Bar Charts #############################
 ################################################################################
 
-# 3. Create your first scatter plot
+# 3. Create your first bar chart
 
-ggplot(mtcars) +
-    geom_point(aes(x = wt, y = mpg))
+ggplot(mpg) +
+    geom_bar(aes(x = class))
 
-# QUESTION: What can you say about the relationship between weight and mpg for a car?
+# QUESTION: What can you say about the different types of cars in the dataset?
 
 
 
-# 4. Changing point color in a ggplot scatter plot
+# 4. Changing bar color in a ggplot bar chart
 
-ggplot(mtcars) +
-    geom_point(aes(x = wt, y = mpg), color = 'blue')
+ggplot(mpg) +
+    geom_bar(aes(x = class), fill = 'blue')
 
 # EXERCISE: Try changing the color to see how that affects your plot.
 # HINT: You can try colors like 'red', 'green', or 'yellow'
@@ -50,162 +48,146 @@ ggplot(mtcars) +
 
 
 
-# 5. Map the variable am (automatic / manual transmission) to a color
-mtcars$am <- factor(mtcars$am)
+# 5. Stacked Bar Chart: Map the variable drv (front-, back-, 4-wheel drive) to a color
 
-ggplot(mtcars) +
-    geom_point(aes(x = wt, y = mpg, color = am))
+ggplot(mpg) +
+    geom_bar(aes(x = class, fill = drv))
+
+# QUESTION: Why does this graph produce multiple colors, when the prior graph produced only one?
+# EXERCISE: Try replacing `drv` with a different variable to see what happens
+# HINT: Try using a categorical variable like trans
+
+
+
+# 6. Stacked Bar Chart: Map the variable cyl (4-, 6-, 8-cylinder engine) to a color
+
+ggplot(mpg) + 
+    geom_bar(aes(x = class, fill = factor(cyl)))
 
 # EXERCISE: Run the two blocks of code below and compare their output
 # HINT: Look at the color scale in the legend!
-# QUESTION: Why did we map the variable am above to a factor? 
+# QUESTION: Why did we map the variable cyl above to a factor? 
+# HINT: Stacked bar charts don't work well with continuous variables
+# HINT: A color scale for a categorical variable is a series of discrete colors
 
 # Code block 1
-data(mtcars)
-ggplot(mtcars) +
-    geom_point(aes(x = wt, y = mpg, color = am))
+
+ggplot(mpg) + 
+    geom_bar(aes(x = class, fill = cyl))
+
 
 # Code block 2
-mtcars$am <- factor(mtcars$am)
-ggplot(mtcars) +
-    geom_point(aes(x = wt, y = mpg, color = am))
+
+ggplot(mpg) + 
+    geom_bar(aes(x = class, fill = factor(cyl)))
 
 
 
-# 6. Changing point shapes in a ggplot scatter plot
-mtcars$am <- factor(mtcars$am)
-ggplot(mtcars) +
-    geom_point(aes(x = wt, y = mpg, shape = am))
+# 7. Dodged Bar Chart: Map the variable cyl (4-, 6-, 8-cylinder engine) to a color
 
-# QUESTION: When might you choose to use shape instead of color?
+ggplot(mpg) + 
+    geom_bar(aes(x = class, fill = factor(cyl)), 
+             position = position_dodge(preserve = 'single'))
 
-
-
-# 7. Changing the size aesthetic mapping in a ggplot scatter plot
-
-ggplot(mtcars) +
-    geom_point(aes(x = wt, y = mpg, size = cyl))
-
-# QUESTION: How does modifying the size in this scatter plot affect readability?
+# EXERCISE: Compare this graph with the stacked bar graph from before
+# QUESTION: Name a comparison that is easier to make with each type of graph
+# HINT: Think about comparisons across vehicle class and across number of cylinders!
 
 
 
-# 8. Changing transparency in a ggplot scatter plot with the alpha aesthetic
-    
-ggplot(mtcars) +
-    geom_point(aes(x = wt, y = mpg, alpha = cyl))
+################################################################################
+################### Creating Bar Charts with a Defined y-axis ##################
+################################################################################
 
-# QUESTION: Do you think this transparency makes this graph easier or harder to understand? Why?
+# 8. Calculate average highway miles per gallon by class
 
+by_hwy_mpg <- mpg %>% group_by(class) %>% summarise(hwy_mpg = mean(hwy))
 
-
-# 9. Changing transparency for all points in the graph
-
-ggplot(mtcars) +
-    geom_point(aes(x = wt, y = mpg, size = cyl), alpha = 0.3)
-
-# QUESTION: How does this transparency improve the legibility of the points in the bottom right?
-# QUESTION: Does this solve the readability issues you identified before?
+# QUESTION: Which car class has the best (highest) highway mpg?
 
 
-# 10. Aesthetic mappings vs. parameters in ggplot
 
-ggplot(mtcars) +
-    geom_point(aes(x = wt, y = mpg), shape = 18)
+# 9. Graph highway mpg by class: introducing stat = 'identity'
 
-ggplot(mtcars) +
-    geom_point(aes(x = wt, y = mpg, shape = am))
+ggplot(by_hwy_mpg) + 
+    geom_bar(aes(x = class, y = hwy_mpg), stat = 'identity')
 
-# QUESTION: What difference do you see in the code above? Why do you think the plots are different?
-# HINT: It's related to including shape either INSIDE or OUTSIDE the aes()!
+# EXERCISE: Try running this code without stat = 'identity'. 
+# QUESTION: What happened?
+# QUESTION: What do you think this error message means?
+# HINT: Check the blog post for more information!
+
+
+
+# 10. Graph highway mpg by class: introducing geom_col
+
+ggplot(by_hwy_mpg) + 
+    geom_col(aes(x = class, y = hwy_mpg))
+
+# EXERCISE: Compare this code to the code above from step 8
+# QUESTION: Which code changes did you find
+# QUESTION: Why are these two graphs the same?
+# HINT: Read the description for geom_col by running ?geom_col in your console
+
+
+
+################################################################################
+################### Aesthetics and Parameters for Bar Charts ###################
+################################################################################
+
+# 11. Introducing the color parameter for a bar chart
+
+ggplot(mpg) +
+    geom_bar(aes(x = class), color = 'blue')
+
+# QUESTION: When might you use color instead of, or in addition to, fill?
+
+
+
+# 12. Applying color and fill parameters to the same chart
+
+ggplot(mpg) +
+    geom_bar(aes(x = class), fill = '#003366', color = '#add8e6')
+
+
+
+# 13. WORKBOOK BONUS: Mapping drv to the color aesthetic
+
+ggplot(mpg) + 
+    geom_bar(aes(x = class, color = drv), fill = 'white')
+
+# QUESTION: Do you prefer this style of stacked bar graph or the filled stacked bar we saw before?
+
+
+
+# 14. WORKBOOK BONUS: Mapping drv to the alpha aesthetic to add transparency
+
+ggplot(mpg) + 
+    geom_bar(aes(x = class, alpha = drv))
+
+# QUESTION: Does this use of alpha improve or reduce the readability of this graph?
+
 
 
 ################################################################################
 ############# Common Errors with Aesthetic Mappings and Parameters #############
 ################################################################################
 
-# 11. Error: Trying to include aesthetic mappings *outside* your `aes()` call
+# 15. Error: Trying to include aesthetic mappings *outside* your `aes()` call
 
-ggplot(mtcars) + geom_point(geom_point(aes(x = wt, y = mpg), shape = cyl))
+ggplot(mpg) + geom_bar(aes(x = class), fill = drv)
 
 # EXERCISE: Why do you think this code returns an error? 
-# HINT: Are we including our call to 'shape' in the right place?
+# HINT: Are we including our call to 'fill' in the right place?
 # QUESTION: How can you fix this error?
 
 
 
 # 12. Error: Trying to specify parameters *inside* your `aes()` call
 
-ggplot(mtcars) + 
-    geom_point(aes(x = wt, y = mpg, color = 'blue'))
-
-# EXERCISE: Why do you think this code returns an error? 
-# HINT: Are we including our call to 'color' in the right place?
-# QUESTION: How can you fix this error?
-
-
-
-
-
-
-
-## Investigating our dataset
-# Add some exploratory code
-
-# Our first bar chart
-library(tidyverse)
-
-ggplot(mpg) +
-geom_bar(aes(x = class))
-
-# Fill in blue
-ggplot(mpg) +
-    geom_bar(aes(x = class), fill = 'blue')
-
-# Stacked bar with fill = drv
-ggplot(mpg) +
-geom_bar(aes(x = class, fill = drv))
-
-# CYL Stacked bar with 4 segments
-# Note we convert the cyl variable to a factor to fill properly
-ggplot(mpg) + 
-    geom_bar(aes(x = class, fill = factor(cyl)))
-
-# Dodged bar chart
-# Note we convert the cyl variable to a factor here in order to fill by cylinder
-ggplot(mpg) + 
-    geom_bar(aes(x = class, fill = factor(cyl)), position = position_dodge(preserve = 'single'))
-
-## Scaling bar size to a variable in your data
-
-# Introduce stat identity
-# Use dplyr to calculate the average hwy_mpg by class
-by_hwy_mpg <- mpg %>% group_by(class) %>% summarise(hwy_mpg = mean(hwy))
-
-ggplot(by_hwy_mpg) + 
-geom_bar(aes(x = class, y = hwy_mpg), stat = 'identity')
-    
-# Demonstrate geom_col
-# Use dplyr to calculate the average hwy_mpg by class
-by_hwy_mpg <- mpg %>% group_by(class) %>% summarise(hwy_mpg = mean(hwy))
-
-ggplot(by_hwy_mpg) + 
-    geom_col(aes(x = class, y = hwy_mpg))
-
-# color
-ggplot(mpg) +
-    geom_bar(aes(x = class), color = 'blue')
-
-# color_and_fill
-ggplot(mpg) +
-    geom_bar(aes(x = class), fill = '#003366', color = '#add8e6')
-
-##### Details of other aesthetics, x, y, fill, color, group, linetype, alpha
-
-##### Trying to include aesthetic mappings *outside* your `aes()` call
-
-ggplot(mpg) + geom_bar(aes(x = class), fill = drv)
-
-##### Trying to specify parameters *inside* your `aes()` call
 ggplot(mpg) + 
     geom_bar(aes(x = class, fill = 'blue'))
+
+# EXERCISE: Why doesn't this code color our graph in blue?
+# HINT: Are we including our call to 'fill' in the right place?
+# QUESTION: How can you fix this issue?
